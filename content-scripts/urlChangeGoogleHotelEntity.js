ba19224865e77c831b155a9b0d14c7942c4e1156
Xetnus <Xetnus@gmail.com>
2021-04-25 01:23:16 -0400

    Support for sponsored hotels

--- content-scripts/urlChangeGoogleHotelEntity.js
+++ content-scripts/urlChangeGoogleHotelEntity.js
@@ -16,14 +16,22 @@ var operateOnPage = function() {
         chrome.storage.local.get({[caseKey]: []}, function(data) {
             var places = data[caseKey];
 
-            var nameElement = document.body.querySelector("h1");
-            var phoneElement = nameElement.parentElement.parentElement.querySelector(":scope > div:nth-child(3) > div > span:nth-child(3)");
+            var overviewNameElement = document.querySelector("#overview[role='tabpanel']").querySelector("h1");
+            var phoneElement = overviewNameElement.parentElement.parentElement.querySelector(":scope > div:nth-child(3) > div > span:nth-child(3)");
             var phone = "";
             if (phoneElement) { phone = phoneElement.innerText; }
 
-            if (places.some(place => place.name === nameElement.innerText && place.phone === phone)) {
+            if (places.some(place => place.name === overviewNameElement.innerText && place.phone === phone)) {
                 var eyeElement = globalThis.createIconElement(" " + globalThis.EYEBALL_UNICODE, globalThis.SEEN_TOOLTIP);
-                nameElement.appendChild(eyeElement);
+                overviewNameElement.appendChild(eyeElement);
+
+                // Sponsored hotels get a special "Sponsored" tab
+                var sponsoredTabElement = document.querySelector("#featured[role='tabpanel']");
+                if (sponsoredTabElement) {
+                    var sponsoredNameElement = sponsoredTabElement.querySelector("header");
+                    var contactElement = sponsoredNameElement.querySelector("div");
+                    sponsoredNameElement.insertBefore(eyeElement.cloneNode(true), contactElement);
+                }
             }
         });
     });
